3e120592cec9b72af001810df712f85d0030a920
Tatiana <mahotina.o@mai.ru>
2026-06-19 20:48:52 +0300

    получение исключения MyArrayDataException и вывод в рельтата в консоли

--- Main.java
+++ Main.java
@@ -1,8 +1,16 @@
 
-class MyArraySizeException extends RuntimeException { // создаем класс исключения, чтобы в последсвии понимать что за ошибка
+class MyArraySizeException
+        extends RuntimeException { // создаем класс исключения, чтобы в последсвии понимать что за ошибка
 
-    public MyArraySizeException(String message) { // коструктор класса
-        super(message);
+    public MyArraySizeException(String message1) { // коструктор класса
+        super(message1);
+    }
+}
+
+class MyArrayDataException extends RuntimeException {// класс исключения, при выявление неверного формата элемента
+
+    public MyArrayDataException(String massage2) {
+        super(massage2);
     }
 }
 
@@ -18,22 +26,43 @@ public class Main {
             }
         }
         System.out.println("Массив 4 на 4");
-        for (int i = 0; i < 4; i++) {// задаем ддлину массива
+        for (int i = 0; i < 4; i++) {// задаем длину массива
             for (int j = 0; j < 4; j++) {
                 System.out.print(array[i][j] + " ");// вывод каждой строки массива
             }
-            System.out.println();// первод новой строки массива на другую строчку
+            System.out.println();// первод строки массива на другую новую строчку
         }
+
+        int sum = 0;
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                try {
+                    int number = Integer.parseInt(array[i][j]); // метод преобразует строку в число
+                    sum += number;// прибпвка каждого элемента к общей сумму
+
+                } catch (NumberFormatException e) {//поймать ошибку если формат не числовой
+                    throw new MyArrayDataException("Элемент массива -" + array[i][j] + " в ячейке[" + i + "][" + j + "]имеет не числовой формат");
+                }
+            }
+        }
+        System.out.println("Сумма всех элементов массива: " + sum);
     }
 
     public static void main(String[] args) {
-        String[][] rightArray = new String[4][4];//массив правильной длины
-        char letter = 'A'; //задаем начальный элемент массива
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                rightArray[i][j] = String.valueOf(letter++); // увеличение длины массива на 1 с кадждым проходом по циклам и перевод массива в строку
-            }
+        String[][] incorrectArray = { // задаем массив
+            {"1", "2", "3", "4"},
+            {"1", "2", "3", "4"},
+            {"1", "ф", "3", "4"},
+            {"1", "2", "3", "4"}
+        };
+        try {
+            createArray(incorrectArray);
+
+        } catch (MyArraySizeException e) {
+            System.out.println("Ошибка:" + e.getMessage()); //вывод исключения , если массив имеет отличный формат от 4 на 4
+        } catch (MyArrayDataException e) {
+            System.out.println("Ошибка:" + e.getMessage());// вывод исключение, если в массиве есть хотя бы один не число
         }
-        createArray(rightArray);
     }
 }
