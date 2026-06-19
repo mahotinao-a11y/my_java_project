5e322e2e4c9ae7e6c8727bf08a0abf08ac45d5ed
Tatiana <mahotina.o@mai.ru>
2026-06-19 12:59:45 +0300

    Неверный формат общего количва строк в массиве

--- Main.java
+++ Main.java
@@ -1,26 +1,33 @@
 
+class MyArraySizeException extends RuntimeException { // создаем класс исключения, чтобы в последсвии понимать что за ошибка
+
+    public MyArraySizeException(String message) { // коструктор класса
+        super(message);
+    }
+}
+
 public class Main {
 
-    public static void createArray(String[][] array) { // метод для вывода массива 4 на 4
-        System.out.println("Массив 4 на 4");
-        for (int i = 0; i < 4; i++) { // цикл внешний 
-            for (int j = 0; j < 4; j++) { // цикл внутренний
-                System.out.print(array[i][j] + " "); // вывод каджого элемента массива
+    public static void createArray(String[][] array) { // метод, который вызываем в конце
+        if (array.length != 4) { // общаяя проверка, что в массисе 4 строки
+            throw new MyArraySizeException("Массив содержит невернное количество строк" + array.length);
+        }
+        for (int i = 0; i < array.length; i++) {//проходим по каждому стороке массива
+            if (array[i].length != 4) {// проверяем длину каждой строчки, проврке по j не нужна, так j -'это элемент в столбце массива, i показывает длину строчки массива и заодно сколько колонок будет
+                throw new MyArraySizeException("Неверное количество столбцов: " + i + "имеет длину " + array[i].length);
             }
-        System.out.println(); // перевод каждой строчки на новую строку
+        }
+        System.out.println("Массив 4 на 4");
+        for (int i = 0; i < 4; i++) {// задаем ддлину массива
+            for (int j = 0; j < 4; j++) {
+                System.out.print(array[i][j] + " ");// вывод каждой строки массива
+            }
+            System.out.println();// первод новой строки массива на другую строчку
         }
     }
-    public static void main(String[] args) { // вход в программу для запуска кода
-        String[][] myArray = {// массив для заполнения значений массива
-            {"E", "F", "G", "H"},
-            {"E", "F", "G", "H"},
-            {"I", "J", "K", "L"},
-            {"M", "N", "O", "P"}
-        };
 
-        createArray(myArray); // вызоа метода
-
+    public static void main(String[] args) {
+        String[][] wrongArray = new String[3][4]; // создаем новый неправльный массив, где количесвто строк 3 , а не 4
+        createArray(wrongArray);
     }
-    }
-    
-    
+}
